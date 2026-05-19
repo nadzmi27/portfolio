5e8bea93a20711245b798402066c3120b69b9af3
nadzmi27 <nadzmiagthomas@gmail.com>
2026-05-20 02:10:56 +1000

    feat: added navigation header

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://nadzmi27.github.io",
-  base: '/portfolio'
+  base: "/portfolio",
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
