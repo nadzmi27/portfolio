91f428f56e9e63d4a4ef6629e5b26ab46d21914c
nadzmi27 <nadzmiagthomas@gmail.com>
2026-07-20 07:27:30 +1000

    fix: build error after installation of astro sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,7 +13,6 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: "https://nadzmi.dev",
-  trailingSlash: "always",
 
   vite: {
     plugins: [tailwindcss()],
